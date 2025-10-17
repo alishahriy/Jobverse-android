8588595853c0e42461943da1a6c6c3c548f19cd5
alishahriy <65369348+alishahriy@users.noreply.github.com>
2025-10-17 04:06:54 +0330

    Add MainActivity.kt

---
+++ .github/workflows/app/src/main/java/com/jobverse/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.jobverse
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface {
+                    Text(text = "Welcome to Jobverse!")
+                }
+            }
+        }
+    }
+}
